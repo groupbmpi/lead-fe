88cad4d38097df1133a8574ab1cb92637764a7d0
Vincent C <vcl3636@gmail.com>
2023-12-20 08:49:06 +0700

    fix: mentor profile picture

--- src/pages/participant/view-module.tsx
+++ src/pages/participant/view-module.tsx
@@ -5,40 +5,40 @@ import Head from 'next/head';
 import { checkAuth } from '@/utils/auth';
 
 const ViewModule = () => {
-    const router = useRouter();
-    const googleSiteLink = 'https://docs.google.com/presentation/d/1mGNTMZ3bZQvcm4DD59sw3fqOuzF_N0MsUyn8dhtmVO8/edit#slide=id.p';
+  const router = useRouter();
+  const googleSiteLink = 'https://docs.google.com/presentation/d/1mGNTMZ3bZQvcm4DD59sw3fqOuzF_N0MsUyn8dhtmVO8/edit#slide=id.p';
 
-    const [allowed, setAllowed] = useState(false);
-    useEffect(() => {
-        const checkAuthentication = async () => {
-            const isAllowed = await checkAuth(['PARTICIPANT']);
-            setAllowed(isAllowed);
+  const [allowed, setAllowed] = useState(false);
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      const isAllowed = await checkAuth(['PARTICIPANT']);
+      setAllowed(isAllowed);
 
-            if (!isAllowed) {
-                router.push('/mentor-login');
-            }
-        };
-        checkAuthentication();
-    });
-    
+      if (!isAllowed) {
+        router.push('/participant-login');
+      }
+    };
+    checkAuthentication();
+  });
+
   return (
     <>
-        <Head>
-            <title>LEAD - View Module</title>
-        </Head>
-        {allowed && <NavbarParticipant />}
-        <div className="vh-100 d-flex flex-column overflow-hidden">
-            <div className="flex-grow-1">
-                <iframe
-                    src={googleSiteLink}
-                    title="Google Site"
-                    width="100%"
-                    height="100%"
-                    style={{ border: 'none' }}
-                    allowFullScreen
-                ></iframe>
-            </div>
+      <Head>
+        <title>LEAD - View Module</title>
+      </Head>
+      {allowed && <NavbarParticipant />}
+      <div className="vh-100 d-flex flex-column overflow-hidden">
+        <div className="flex-grow-1">
+          <iframe
+            src={googleSiteLink}
+            title="Google Site"
+            width="100%"
+            height="100%"
+            style={{ border: 'none' }}
+            allowFullScreen
+          ></iframe>
         </div>
+      </div>
     </>
   );
 };
